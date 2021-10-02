ff2598a34cd59b6bb32ceb23bf6792d114eb4679
sarah-mosbah <sarahmosbah101@gmail.com>
2021-10-02 14:45:01 +0200

    Inorder Traverse

---
+++ easy/inOrderTraverseBinaryTree.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var inorderTraversal = function(root) {
+  
+    let result = [];
+    
+    const traverse = (root) => {
+        if(!root) return;
+        
+        traverse(root.left)
+        result.push(root.val);
+        traverse(root.right);
+        
+    }
+    traverse(root);
+    return result;
+   
+};
