801f9615b9045c94766e13efee114d2239c86d7a
sarah-mosbah <sarahmosbah101@gmail.com>
2021-11-20 00:33:37 +0200

    BitWise Power Of 2

--- easy/powerOfTwo.ts
+++ easy/powerOfTwo.ts
@@ -11,3 +11,10 @@ function isPowerOfTwo(n: number): boolean {
     } 
     return false;
 };
+
+
+function isPowerOfTwoInBit(n: number): boolean {
+
+    if(n<=0) return false;
+    return !(n&(n-1));
+};
