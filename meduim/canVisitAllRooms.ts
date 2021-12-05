1e45bbd5fd6b7a88939e68a2b5afaace1ee8c715
sarah-mosbah <sarahmosbah101@gmail.com>
2021-12-05 21:47:52 +0200

    Can Visit ALl Rooms

---
+++ meduim/canVisitAllRooms.ts
@@ -0,0 +1,16 @@
+function canVisitAllRooms(rooms: number[][]): boolean {
+    const visitedRooms = [];
+    const bfsQueue = [0];
+    let count = 1;
+    visitedRooms[0] = true;
+    while (bfsQueue.length) {
+     let keys = rooms[bfsQueue.shift()];
+     for (let k of keys)
+       if (!visitedRooms[k]) {
+           bfsQueue.push(k);
+           visitedRooms[k] = true, count++;
+       }
+     }
+   return count === rooms.length;
+ 
+ };
