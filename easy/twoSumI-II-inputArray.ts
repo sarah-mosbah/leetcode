1f7b2c39007e9659e0da331b9d2d382ca2857785
sarah-mosbah <sarahmosbah101@gmail.com>
2022-01-03 21:50:48 +0200

    Sort in Two Numbers

--- easy/twoSumI-II-inputArray.ts
+++ easy/twoSumI-II-inputArray.ts
@@ -1,8 +1,9 @@
 function twoSum(numbers: number[], target: number): number[] {
+    numbers.sort((x, y) => x - y); // O(n log(n));
     let low = 0;
      let high = numbers.length-1;
      
-     while(low <=  high) {
+     while(low <  high) {
          const sum = numbers[low] + numbers[high];
          if(sum > target) high--;
          else if (sum === target) break;
