da1bcf31ce1ccad01dc0ec3fe737fd2a9e50039d
sarah-mosbah <sarahmosbah101@gmail.com>
2022-01-03 22:53:46 +0200

    Enhancing is Subsequence

--- easy/isSubsequence.ts
+++ easy/isSubsequence.ts
@@ -1,17 +1,14 @@
 
 function isSubsequence(s: string, t: string): boolean {
-
-    let counter = 0;
 	let pointerA = 0;
 	let pointerB = 0;
 	while (pointerA < t.length) {
 		if(t[pointerA] === s[pointerB]) {
-			counter++;
 			pointerA++;
 			pointerB++;
 		} else {
 			pointerA++;
 		}
 	}
-  return s.length === counter;
+  return s.length === pointerB;
 };
